c978fdbb7c22af0f260e45e4f43f0c0e68602d27
kittzaza <97072839+kittzaza@users.noreply.github.com>
2024-01-26 11:46:34 +0700

    Create Exercise4_Weekit_P
    Create Exercise4_Weekit_P

---
+++ Exercise4_Weekit_P.py
@@ -0,0 +1,15 @@
+Foundation_English = 78.5
+General_Business = 75.5
+Introduction_to_Computer_Systems = 72.3
+Computer_Programming = 74.5
+
+
+print("--- Your Score ---")
+
+print(f"Foundation English : {Foundation_English}")
+
+print(f"General Business :  {General_Business}")
+
+print(f"Introduction to Computer Systems :  {Introduction_to_Computer_Systems}้")
+
+print(f"Computer Programming :  {Computer_Programming}")
